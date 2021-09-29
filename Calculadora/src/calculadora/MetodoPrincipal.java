ab8b95d5f8ae167b6a2aeee0318fa64cd38ab873
Pedro Santos <pedrohrsantos01@gmail.com>
2021-09-29 09:16:35 -0300

    Calculadora Java

---
+++ Calculadora/src/calculadora/MetodoPrincipal.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package calculadora;
+/**
+ *
+ * @author backstageel
+ */
+public class MetodoPrincipal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MinhaCalculadora calc = new MinhaCalculadora();
+        calc.setVisible(true);
+        
+
+    }
+
+}
